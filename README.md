bad0af396197a31e367e344dd4a735653c74932a
Landcruiser87 <andyheroy@gmail.com>
2024-02-29 20:49:54 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,76 @@
+<h1 align="center">
+  <b>Machine Learning survey</b><br>
+</h1>
+
+<p align="center">
+      <a href="https://www.python.org/">
+        <img src="https://img.shields.io/badge/Python-3.8-ff69b4.svg" /></a>    
+</p>
+
+
+## Purpose
+
+The purpose of this repo is to perform a yearly survey of major machine learning conferences.  Extract all the metadata, abstracts, and other information from of all the papers and look for topic frequencies that show up.  Modeling ossibilities are clustering, cosine similarity to a particular subject.
+
+
+## Requirements
+- Python >= 3.8
+
+## Main Libraries used
+- numpy
+- pandas
+- rich
+- requests
+- matplotlib
+
+Order of operations of below terminal commands. 
+- Open Terminal
+- Clone repo
+- Change directories
+- Create venv
+- Activate venv
+- Install libraries
+
+```
+`CTRL + SHIFT + ~` will open a terminal
+Navigate to the directory where you want to clone the repo. 
+
+## Cloning and setting up environment.
+Launch VSCode if that is IDE of choice.
+ 
+$ git clone https://github.com/landcruiser87/compare_abstracts.git
+$ cd compare_abstracts
+$ python -m venv .c_venv
+(Or replace ".c_venv" with whatever you want to call your environment)	
+
+On Windows
+$ .ca_venv\Scripts\activate.bat
+
+On Mac
+$ source .c_venv/bin/activate
+```
+
+Before next step, ensure you see the environment name to the left of your command prompt.  If you see it and the path file to your current directory, then the environment is activated.  If you don't activate it, and start installing things.  You'll install all the `requirements.txt` libraries into your base python environment. Which will lead to dependency problems down the road.  I promise.
+
+![Screenshot 2023-03-28 144052](https://user-images.githubusercontent.com/16505709/228358535-3364e0ea-b273-40b8-ab59-4dddf2f92ee2.png)
+
+```
+$ pip install -r requirements.txt
+```
+
+## File Setup
+While in root directory run commands below
+
+```
+$ mkdir data
+$ cd data
+$ mkdir logs
+$ mkdir scraped
+$ cd ..
+```
+
+# Project Todo list
+- [ ] stuff 
+- [ ] stuff stuff
+- [ ] stuff stuff
+- [ ] stuff stuff
---
+++ scripts/scrape_abstracts.py
@@ -0,0 +1,172 @@
+import numpy as np
+import pandas as pd
+from rich.logging import RichHandler
+from rich.console import Console
+from time import strftime, sleep
+from collections import deque
+import os
+import logging
+import requests
+import json
+import time	
+# from bs4 import BeautifulSoup, only installing if we need it. 
+
+#Grab abstracts from here. 
+#https://neurips.cc/virtual/2023/papers.html?filter=titles
+
+#Logging setup stuffs
+current_date = strftime("%m-%d-%Y_%H-%M-%S")
+FORMAT = "%(asctime)s|%(levelname)s|%(funcName)s|%(lineno)d|%(message)s" #[%(name)s]
+FORMAT_RICH = "%(funcName)s|%(lineno)d|%(message)s"
+console = Console(color_system="truecolor")
+rh = RichHandler(level = logging.WARNING, console=console)
+rh.setFormatter(logging.Formatter(FORMAT_RICH))
+
+#Set up basic config for logger
+logging.basicConfig(
+	level=logging.INFO, 
+	format=FORMAT,
+	datefmt="[%X]",
+	handlers=[
+		rh,
+		# logging.FileHandler(f"./data/logs/{current_date}.log", mode="w")
+	]
+)
+logger = logging.getLogger(__name__) 
+
+CONF_DICT={
+	"neurips":{
+		"name":"Conference and Workshop on Neural Information Processing Systems",
+		"abbrv":"NEURIPS",
+		"url":"https://neurips.cc/static/virtual/data/neurips-2023-orals-posters.json",
+		"headers":{
+			"authority":"https://neurips.cc/static/virtual/data/neurips-2023-orals-posters.json",
+			"method":"GET",
+			"path":"/static/virtual/data/neurips-2023-orals-posters.json",
+			"scheme":"https",
+			"accept":"*/*",
+			"accept-encoding":"gzip,deflate,br,zstd",
+			"accept-language":"en-US,en;q=0.9",
+			"referer":"https://neurips.cc/virtual/2023/papers.html?filter=titles",
+			"sec-ch-ua":'"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+			"sec-ch-ua-Mobile":"?0",
+			"sec-ch-ua-platform":"Windows",
+			"sec-fetch-dest":"empty",
+			"sec-fetch-mode":"cors",
+			"sec-fetch-site":"same-origin",
+			"user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+			"x-requested-with":"XMLHttpRequest"
+			}
+	},
+	"icml":{
+		"name":"International Conference of Machine Learning",
+		"abbrv":"ICML",
+		"url":"stuff",
+		"headers":"morestuff"
+	},
+	"ml4h":{
+		"name":"Machine Learning for Health",
+		"abbrv":"ML4H",
+		"url":"stuff",
+		"headers":"morestuff"
+	}
+}
+
+#Will need URL and a dict to store each request header
+#FUNCTION log time
+def log_time(fn):
+	def inner(*args, **kwargs):
+		tnow = time.time()
+		out = fn(*args, **kwargs)
+		te = time.time()
+		took = round(te - tnow, 2)
+		if took <= 60:
+			logging.info(f"{fn.__name__} ran in {took:.2f}s")
+		elif took <= 3600:
+			logging.info(f"{fn.__name__} ran in {(took)/60:.2f}m")		
+		else:
+			logging.info(f"{fn.__name__} ran in {(took)/3600:.2f}h")
+		return out
+	return inner
+
+#FUNCTION Filter result
+def filter_neurips_result(json_data:json)->dict:
+	# Possible cool data within neuripps
+	ids = list(range(json_data["count"]))
+	base_keys = ["id","name","author","abstract","topic","session","event_type","virtualsite_url","url","paper_url"]
+	base_dict = {val:{key:"" for key in base_keys} for val in ids}
+	for id in ids:
+		authors = range(len(json_data['results'][id]['authors']))
+		base_dict[id]["author"] = {author:{} for author in authors} 
+		#Grab author info
+		for author in authors:
+			auth_info = json_data["results"][id]["authors"][author]
+			base_dict[id]["author"][author].update(**auth_info)
+		
+		#Grab other keys we want from the paper
+		for term in base_keys:
+			if json_data["results"][id].get(term):
+				base_dict[id][term] = json_data["results"][id][term]
+
+	return base_dict
+
+	#could be cool stuff in event media, but save for later. 
+
+#FUNCTION Request Conference
+def request_conf(conference:str):
+	url = CONF_DICT[conference]["url"]
+	headers = CONF_DICT[conference]["headers"]
+	resp = requests.get(url, headers=headers)
+
+	if resp.status_code == 200:
+		resp_json = resp.json()
+		results = filter_neurips_result(resp_json)
+	else:
+		logger.warning(f"\nServer Error\n{url}\nResponse: {resp.status_code}\n{resp.reason}")
+		results = None
+
+	return results
+
+#FUNCTION save results
+def save_result(name:str, processed:pd.DataFrame):
+	"""Save Routine.  Takes in name of file and processed dataframe.  Saves it to the data/cleaned folder.  Also offloads the updated CM_dict and PCS_dict variables to JSON for future use. 
+
+	Args:
+		name (str): Name of file being converted
+		processed (pd.DataFrame): Processed dataframe of translated codes
+	"""	
+	spath = f"./data/cleaned/web/{name}"
+	processed.to_csv(spath, mode='w', header=0, encoding='utf-8')
+	logger.info(f"CSV for file {spath} saved")
+
+#FUNCTION save dictionary
+def save_data(data):
+	result_json = json.dumps(data, indent=4)
+	with open(f"./data/json/{data}.json", "w") as outf:
+		outf.write(result_json)
+	logger.info("data saved")
+
+
+#NOTE START PROGRAM
+#FUNCTION main
+@log_time
+def main():
+	"""Main driver code for program
+	"""	
+	logger.info(f"Beginning search")
+	main_conferences = ["neurips", "icml", "ml4h"] 
+	#TODO - Add MHSRS when ready, that website will be more challenging to scrape
+
+	for conference in main_conferences[:1]:
+		result = request_conf(conference)
+		# add_tovectordb(result)
+		# save Dataframes
+		# save_result(name, processed)
+		logger.warning(f"{conference} has been converted and saved")
+
+	# save_data()		
+	logger.warning(f'All conferences have been searched.  Shutting down program')
+
+if __name__ == "__main__":
+	main()
+
